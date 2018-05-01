5034abe7357c1c8b9f786fc90c4633f0c4dbc280
crazyzlj <crazyzlj@gmail.com>
2018-05-01 19:40:20 +0800

    update

--- FindPackages.cmake
+++ FindPackages.cmake
@@ -58,7 +58,6 @@ ENDIF()
 IF(WITH_OPENMP)
   FIND_PACKAGE(OpenMP)
   IF(OPENMP_FOUND)
-    ADD_DEFINITIONS(-DSUPPORT_OMP)
     SET(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${OpenMP_C_FLAGS}")
     SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OpenMP_CXX_FLAGS}")
     geo_list_unique(CMAKE_C_FLAGS CMAKE_CXX_FLAGS)
@@ -66,10 +65,4 @@ IF(WITH_OPENMP)
     MESSAGE(WARNING "CMake fails to determine OpenMP.
 Please check your compiler to make sure the support of OpenMP.")
   ENDIF()
-ENDIF()
-
-### Bson and MongoC.
-IF (WITH_MONGOC)
-  INCLUDE(FindBson)
-  INCLUDE(FindMongoC)
 ENDIF()
